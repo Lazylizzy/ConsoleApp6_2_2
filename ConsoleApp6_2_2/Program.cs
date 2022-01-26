5920b99632ea3068ddc8809a169c5763b50912e3
lazylizzy <petrovalizpetrova@yandex.ru>
2022-01-26 22:04:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6_2_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            string startString = Console.ReadLine(); 
+            startString = startString.Replace(" ", string.Empty); 
+            startString = startString.ToLower(); 
+            string newString = new string(startString.Reverse().ToArray()); 
+            if (startString == newString)
+            {
+                Console.WriteLine("Данное предложение палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Данное предложение не палиндром");
+            }
+            Console.ReadKey();
+        }
+    }
+}
